1b1613db95734b9ae8b12340fe46feb411ba22ab
c-usher <usher.cody@outlook.com>
2021-06-02 09:16:35 -0600

    added folders to ceate a add unit form

---
+++ src/pages/new-unit/Add_Unit_page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+
+export const AddUnit = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    Form Here
+                </Col>
+            </Row>
+       </Container>
+    )
+}
