38c0ec95be38323ce9f95c549cc30c92fe78286d
c-usher <usher.cody@outlook.com>
2021-06-03 19:21:11 -0600

    the search bar now takes input and saves it to a state

--- src/components/search/Search_Comp.js
+++ src/components/search/Search_Comp.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import { Form, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
+import { PropTypes } from "prop-types";
 
-export const SearchComp = () => {
-    return (
-        <div>
-            <Form>
-                <Form.Group as={Row}>
-                    <Form.Label column ms="2">Search: </Form.Label>
-                    <Col ms="9">
-                        <Form.Control
-                            name="searchInp"
-                            placeholder="Search ..."
-                         />   
-                    </Col>
-                </Form.Group>
-            </Form>
-        </div>
-    )
-}
+export const SearchComp = ({handleOnChange, str}) => {
+    console.log(str);
+  return (
+    <div>
+      <Form>
+        <Form.Group as={Row}>
+          <Form.Label column ms="2">
+            Search:{" "}
+          </Form.Label>
+          <Col ms="9">
+            <Form.Control
+              name="searchInp"
+              onChange={handleOnChange}
+              value={str}
+              placeholder="Search ..."
+            />
+          </Col>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+SearchComp.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  str: PropTypes.string.isRequired,
+};
--- src/pages/show_units/Show_Units_page.js
+++ src/pages/show_units/Show_Units_page.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import {Row, Col, Container, Button} from 'react-bootstrap'
-import { SearchComp } from '../../components/search/Search_Comp'
+import React, { useState, useEffect } from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import { SearchComp } from "../../components/search/Search_Comp";
 
 export const ShowUnitsPage = () => {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Button variant="info">Add New Unit</Button>
-                </Col>
-                <Col className="text-right"><SearchComp /></Col>
-        </Row>
-      </Container>
-    );
-}
+    const [str, setStr] = useState("");
+    useEffect(() => { }, [str]);
+
+    
+    const handleOnChange = (e) => {
+      setStr(e.target.value)
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Button variant="info">Add New Unit</Button>
+        </Col>
+        <Col className="text-right">
+          <SearchComp handleOnChange={handleOnChange} str={str} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
