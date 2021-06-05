74bea4277dec90557af93f7699343dde6ec811e2
c-usher <usher.cody@outlook.com>
2021-06-05 11:54:58 -0600

    the show_unit_page is now showing the dummy data

--- src/pages/show_unit/Show_Unit_Page.js
+++ src/pages/show_unit/Show_Unit_Page.js
@@ -2,27 +2,52 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import units from "../../assets/data/dummy_data.json";
 
-const unit = units[0];
+const unit = units[3];
 
 export const ShowUnitPage = () => {
   return (
     <Container>
       <Row>
         <Col>
-          <div className="unit-num">UNIT NUMBER GOES HERE</div>
-          <div className="building">BUILDING GOES HERE</div>
-          <div className="managed">MANAGED GOES HERE</div>
-          <div className="rental">RENTAL GOES HERE</div>
-          <div className="in-house">IN HOUSE GOES HERE</div>
-          <div className="robe-count">ROBE COUNT GOES HERE</div>
-          <div className="inspected">INSPECTED GOES HERE</div>
-          <div className="notes">NOTES GOES HERE</div>
+          <h1 className="unit-num">Unit Number: {unit.unit}</h1>
+          <h2 className="building">Building: {unit.building}</h2>
+          <h2 className="managed">
+            Managed:{" "}
+            {unit.managed ? "This unit is managed" : "This unit is not managed"}
+          </h2>
+          <h2 className="rental">
+            Rental:{" "}
+            {unit.rental
+              ? "This unit is a rental"
+              : "This unit is not a rental"}
+          </h2>
+          <h2 className="in-house">
+            In House:{" "}
+            {unit.inHouse
+              ? "This unit is in house"
+              : "This unit is not in house"}
+          </h2>
+          <h2 className="robe-count">Robes: {unit.robeCount}</h2>
+          <h2 className="inspected">
+            Inspected:{" "}
+            {unit.inspected
+              ? "This unit is inspected"
+              : "This unit is not inspected"}
+          </h2>
+          <h2 className="notes">Notes: {unit.notes}</h2>
+          {/* ! remove this \/ */}
+          <hr />
+          <br />
+          <br />
+          <h1>{JSON.stringify(unit)}</h1>
+          {/* ! remove this /\ */}
         </Col>
       </Row>
     </Container>
   );
 };
 
+//        ! REMOVE THIS \/
 /*        <th>Unit Number</th>
           <th>Building</th>
           <th>Managed</th>
