b9146c5764d9145f480fa9f4b2dbf979c7ccaf2e
c-usher <usher.cody@outlook.com>
2021-06-05 19:23:03 -0600

    the private routes now work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import { DefaultLayout } from "./layout/Default_Layout";
 import { ShowUnitPage } from "./pages/show_unit/Show_Unit_Page";
 import { Dashboard } from "./pages/dashboard/dashboard_page";
 import { LoginPage } from "./pages/login/Login_Page";
 import { AddUnit } from "./pages/new-unit/Add_Unit_page";
 import { ShowUnitsPage } from "./pages/show_units/Show_Units_Page";
+import { PrivateRoute } from "./components/private-route/Private_Route";
 
 function App() {
   return (
@@ -16,20 +16,18 @@ function App() {
           <Route exact path="/">
             <LoginPage />
           </Route>
-          <DefaultLayout>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/add-unit">
-              <AddUnit />
-            </Route>
-            <Route path="/units">
-              <ShowUnitsPage />
-            </Route>
-            <Route path="/unit/:uid">
-              <ShowUnitPage />
-            </Route>
-          </DefaultLayout>
+          <PrivateRoute path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
+          <PrivateRoute path="/add-unit">
+            <AddUnit />
+          </PrivateRoute>
+          <PrivateRoute path="/units">
+            <ShowUnitsPage />
+          </PrivateRoute>
+          <PrivateRoute path="/unit/:uid">
+            <ShowUnitPage />
+          </PrivateRoute>
         </Switch>
       </Router>
     </div>
---
+++ src/components/private-route/Private_Route.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { DefaultLayout } from "../../layout/Default_Layout";
+
+const isValid = false;
+export const PrivateRoute = ({ children, ...rest }) => {
+  return <Route render={() => (isValid ?  <DefaultLayout>{children} </DefaultLayout>: <Redirect to="/" />)} />;
+};
