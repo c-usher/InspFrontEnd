343461400f102b68f1f0552f6b0471bfb2bf2193
c-usher <usher.cody@outlook.com>
2021-08-10 20:28:05 -0600

    created a registration form for users to create an account

--- src/components/registration/Reg_Form.js
+++ src/components/registration/Reg_Form.js
@@ -17,27 +17,39 @@ const RegForm = () => {
               <Form.Label>Full Name</Form.Label>
               <Form.Control type="text" placeholder="Full Name" />
             </Form.Group>
-
             <Form.Group>
               <Form.Label>Phone</Form.Label>
               <Form.Control type="number" placeholder="Phone Number" />
             </Form.Group>
-
             <Form.Group>
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Email" />
             </Form.Group>
-
             <Form.Group>
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-
             <Form.Group>
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control type="password" placeholder="Confirm Password" />
             </Form.Group>
-
+            <ul className="py-4">
+              <li className="text-danger">
+                Password needs to be at least 7 characters long!
+              </li>
+              <li className="text-danger">
+                Password needs at least one uppercase letter!
+              </li>
+              <li className="text-danger">
+                Password needs at least one lowercase letter!
+              </li>
+              <li className="text-danger">
+                Password needs at least one number!
+              </li>
+              <li className="text-danger">
+                Password needs at least one special character!
+              </li>
+            </ul>
             <Button variant="primary" type="submit">
               Submit
             </Button>
